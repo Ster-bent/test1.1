8d86eebad38f95fef1ac2b9b42f57c885860a1c0
Ster-bent <104021908+Ster-bent@users.noreply.github.com>
2023-03-14 19:54:31 +0000

    kotlin goofy

--- hello_worldkot.kt
+++ hello_worldkot.kt
@@ -1,3 +1,9 @@
-fun printman(){
-    printIn("goofball")
+fun printmath(){
+    val x = 25
+    val y = 67
+    printIn(x+y)
+    val ifstatment = when {
+        x > 24 -> "above 24"
+        y < 67 -> "below 67"
+    }
 }
